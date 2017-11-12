a7a9c0ee232576b97a154c0841523131434d3768
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2017-11-12 15:04:28 -0500

    Prompt written.

---
+++ src/java_prg_05_06_celsius_temperature_table/JAVA_PRG_05_06_Celsius_Temperature_Table.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java_prg_05_06_celsius_temperature_table;
+
+/**
+ *
+ * @author bluebackdev
+ * The formula for converting a temperature from Fahrenheit to Celsius is:
+ * 
+ *      C = (5/9) * (F - 32)
+ * 
+ * where F is the Fahrenheit temperature and C is the Celsius temperature.
+ * Write a method named celsius that accepts a Fahrenheit temperature as an
+ * argument. The method should return the temperature, converted to Celsius.
+ * Demonstrate the method by calling it in a loop that displays a table of the
+ * Fahrenheit temperatures 0 through 20 and their Celsius equivalents.
+ */
+public class JAVA_PRG_05_06_Celsius_Temperature_Table {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        // TODO code application logic here
+    }
+    
+}
